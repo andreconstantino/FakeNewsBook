e1fd6c67f340dc85405fb6a69d9f8c46db47bd83
DESKTOP-89A8OS7\guilh <gui.urcelino@gmail.com>
2021-06-23 15:03:35 -0300

    frame primeira visita e frame noticia design

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <style>
+
+        .container-gui{            
+            margin-bottom: 30px;
+        }
+        .title{
+            font-size: 2rem;
+            padding-bottom:0px;
+            margin-bottom:0px;
+        }
+        .subtitle{
+            font-size: 1.50rem;
+        }
+        .button-gui{
+            margin-top:20px;           
+        }
+        h1{
+            margin-bottom:0px; 
+        }
+        .title:not(:last-child){
+            margin-bottom:10px;
+        }
+        .container-gui img{
+            width:100%;
+            height: 150px;
+            background-color:#C1CDC1;
+            margin-top:10px;
+
+        }
+        
+    
+    </style>
+
+  </head>
+  <body>
+  <section class="section">
+    <div class="container-gui">
+        <h1 class="title">
+            Lorem ipisum dolor sit amet
+        </h1>
+        <span class="tag is-success is-large">Notícia verdadeira</span>
+        <img src="https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png" alt="Italian Trulli">
+        <p class="subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mattis enim dolor, ut egestas nunc tincidunt ac. Nullam tempus, ex nec cursus fermentum.
+        </p>
+        <p class="subtitle">
+            Consectetur adipiscing elit mattis enim.
+        </p>
+        <hr/>
+        <div style=" display:flex; justify-content:space-between">
+            <div>
+            <span class="icon-text">
+                <span class="icon">
+                    <i class="far fa-calendar-alt"></i>
+                </span>
+                <span>14/06/2021</span>
+            </span>
+            <br/>
+            <span class="icon-text">
+                <span class="icon">
+                    <i class="far fa-eye"></i>
+                </span>
+                <span>1.520</span>
+            </span>
+            </div>
+            
+            <div>
+            <p>Compartilhar:</p>
+            <p class="buttons">
+                
+                <button class="button" style="background-color:#8b9dc3">
+                    <span class="icon">
+                        <i class="fab fa-facebook"></i>
+                    </span>
+                    
+                </button>
+                <button class="button" style="background-color:#25D366">
+                    <span class="icon is-small">
+                        <i class="fab fa-whatsapp"></i>
+                    </span>
+                    
+                </button>
+            </p>
+            <div>
+        </div>
+
+        
+
+        
+
+  
+    </div>
+  </section>
+  </body>
+</html>
---
+++ primeira-visita.php
@@ -0,0 +1,56 @@
+
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+
+    <style>
+        .container-gui{
+            
+            margin-bottom: 30px;
+        }
+        .title{
+            font-size: 3rem;
+        }
+        .subtitle{
+            font-size: 1.75rem;
+        }
+        .button-gui{
+            margin-top:20px;           
+        }
+    </style>
+
+
+
+  </head>
+  <body>
+  <section class="section">
+    <div class="container-gui">
+      <h1 class="title">
+        Verificador de fake news
+      </h1>
+    </div>
+    <div class="container-gui">
+      <p class="subtitle">
+        Saiba quais são as fake news mais circuladas em cada estado.
+      </p>
+    </div>
+    <div class="container-gui">
+      <p class="subtitle">
+        Obtenha uma lista de notícias e informações em um mapa.
+      </p>
+    </div>
+    <div class="container-gui">
+        <center>
+            <button class="button is-link is-large button-gui">Começar</button>
+        </center>
+    </div>
+    
+  </section>
+  </body>
+</html>

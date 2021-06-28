7823bad4214e95edaae8138a9381baac74b479fd
DESKTOP-89A8OS7\guilh <gui.urcelino@gmail.com>
2021-06-28 19:41:42 -0300

    pagina principal adicionada

---
+++ principal.php
@@ -0,0 +1,158 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <style>
+        *{
+            box-sizing: border-box;
+        }
+
+        .field:not(:last-child) {
+            margin-bottom: 0px;
+        }
+        
+        .map-gui{
+            width:100%;
+            height:250px;
+        }
+        .container-gui{
+            padding: 10px;
+        }
+        .img-mini-noticia{
+            width:100%;
+            height:100%;
+        }
+        .mini-box-noticia{
+            width:100%;
+            height:100px;
+            border: 1px solid #000000;
+            display:flex;
+            margin-bottom:10px;
+
+
+
+
+        }
+        .title-gui{
+
+            font-size:20px;
+        }
+        .subtitle-gui{
+            font-size:10px;
+        }
+    </style>
+
+</head>
+<body>
+
+
+
+  
+
+      
+<div class="field has-addons">
+  <div class="control is-expanded">
+  <div class="control has-icons-left ">
+    <input class="input " type="email" placeholder="Normal">
+    <span class="icon is-left">
+        <i class="fas fa-search"></i>
+    </span>   
+  </div>
+  </div>
+  <p class="control">
+        <span class="select ">
+        <select>
+            <option>SP</option>
+            <option>RJ</option>            
+        </select>
+        </span>
+    </p>
+    <p class="control">
+        <span class="select ">
+        <select>
+            <option>7 dias</option>
+            <option>30 dias</option>
+            <option>100 dias</option>
+        </select>
+        </span>
+    </p>
+  
+</div>
+
+<img class="map-gui" src="https://lh3.googleusercontent.com/OOy8crH8HuezjHqrIEUvDnIjwO5v8A5JWwpz6SwQs-p3zvR54BARyVMUDCJlNTiDZfA=w374-rwa"/>
+
+<section class="container-gui">    
+        <h1 class="title">
+        Lorem ipisum dolor sit amet
+        </h1>  
+        <div class="mini-box-noticia">
+
+            <div style="width:25%;">
+            <img class="img-mini-noticia" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqyQKkEnsHS8Rl3hq9GjAlN712j9ZTWL16TZcZzbmCM-Pf2GaUT97Gv2ScPetyS7xQnME&usqp=CAU"/>
+
+            </div>
+            <div style="width:75%;  padding:10px; overflow: hidden; text-overflow: ellipsis; ">
+  
+            <h1 class="title " style="font-size:20px;">
+            bla bla bla bals lba labla lgfdsadf dddddd bal bal a bla bal bal balbalba balba bal b alb albalb al bal balbalba balba bal b alb albalb
+            </h1>
+           
+            </div>
+            
+        </div>  
+        <div class="mini-box-noticia">
+
+<div style="width:25%;">
+<img class="img-mini-noticia" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqyQKkEnsHS8Rl3hq9GjAlN712j9ZTWL16TZcZzbmCM-Pf2GaUT97Gv2ScPetyS7xQnME&usqp=CAU"/>
+
+</div>
+<div style="width:75%;  padding:10px; overflow: hidden; text-overflow: ellipsis; ">
+
+<h1 class="title " style="font-size:20px;">
+bla bla bla bals lba labla lgfdsadf dddddd bal bal a bla bal bal balbalba balba bal b alb albalb al bal balbalba balba bal b alb albalb
+</h1>
+
+</div>
+
+</div> 
+<div class="mini-box-noticia">
+
+<div style="width:25%;">
+<img class="img-mini-noticia" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqyQKkEnsHS8Rl3hq9GjAlN712j9ZTWL16TZcZzbmCM-Pf2GaUT97Gv2ScPetyS7xQnME&usqp=CAU"/>
+
+</div>
+<div style="width:75%;  padding:10px; overflow: hidden; text-overflow: ellipsis; ">
+
+<h1 class="title " style="font-size:20px;">
+bla bla bla bals lba labla lgfdsadf dddddd bal bal a bla bal bal balbalba balba bal b alb albalb al bal balbalba balba bal b alb albalb
+</h1>
+
+</div>
+
+</div> 
+<div class="mini-box-noticia">
+
+<div style="width:25%;">
+<img class="img-mini-noticia" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqyQKkEnsHS8Rl3hq9GjAlN712j9ZTWL16TZcZzbmCM-Pf2GaUT97Gv2ScPetyS7xQnME&usqp=CAU"/>
+
+</div>
+<div style="width:75%;  padding:10px; overflow: hidden; text-overflow: ellipsis; ">
+
+<h1 class="title " style="font-size:20px;">
+bla bla bla bals lba labla lgfdsadf dddddd bal bal a bla bal bal balbalba balba bal b alb albalb al bal balbalba balba bal b alb albalb
+</h1>
+
+</div>
+
+</div> 
+        
+</section>
+
+
+
+</body>
+</html>
